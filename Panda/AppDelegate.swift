223eb10ae68708c181c550c02abaf6da6506e5cb
矢野史洋 <a2.mj.pop@gmail.com>
2016-08-03 22:11:29 +0900

    add ストーリーボードを使わない形に変更

--- Panda/AppDelegate.swift
+++ Panda/AppDelegate.swift
@@ -10,12 +10,23 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    let mainTabBarController: MainTabbarController = MainTabbarController()
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        self.window!.rootViewController = mainTabBarController
+        
+        self.window!.makeKeyAndVisible()
+        
+        //全体のNavigationBarの色を変更する
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().barTintColor = UIColor.hex("3ca97f", alpha: 1.0)
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.whiteColor()]
+        
         return true
     }
 
