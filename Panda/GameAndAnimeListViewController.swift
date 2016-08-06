0ac2242655889325d68121604f9a8f33b0da3df4
矢野史洋 <a2.mj.pop@gmail.com>
2016-08-06 20:42:17 +0900

    add スポーツのカテゴリーのビューを作成

---
+++ Panda/GameAndAnimeListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GameAndAnimeListViewController.swift
+//  Panda
+//
+//  Created by 矢野史洋 on 2016/08/06.
+//  Copyright © 2016年 矢野史洋. All rights reserved.
+//
+
+import Foundation
---
+++ Panda/SportListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SportListViewController.swift
+//  Panda
+//
+//  Created by 矢野史洋 on 2016/08/04.
+//  Copyright © 2016年 矢野史洋. All rights reserved.
+//
+
+import UIKit
+
+class SportListViewController: BaseQuestionnaireListViewController {
+    
+    @IBOutlet weak var questionnaireCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionnaireCollectionView.registerNib(UINib(nibName: "BaseQuestionnaireListCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+        
+        questionnaireCollectionView.delegate = self
+        questionnaireCollectionView.dataSource = self
+        
+        // CollectionView のコンテンツに余白を付ける（下50px）
+        questionnaireCollectionView.contentInset = UIEdgeInsets(top: 42, left: 0, bottom: 10, right: 0)
+        // CollectionView のスクロール可能範囲に余白を付ける（下50px）
+        questionnaireCollectionView.scrollIndicatorInsets = UIEdgeInsets(top: 42, left: 0, bottom: 10, right: 0)
+    }
+
+}
